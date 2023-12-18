eb5eb3d681a6ff1b88e926086f63e551d7da3cea
imryuik <imahmedtidiane@gmail.com>
2023-12-18 11:36:33 +0000

    some update

--- src/main/java/com/imryuik/crowdfunding/entity/User.java
+++ src/main/java/com/imryuik/crowdfunding/entity/User.java
@@ -1,7 +1,7 @@
 package com.imryuik.crowdfunding.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.NonNull;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,10 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-@Data
 @Entity
-@RequiredArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -46,6 +43,18 @@ public class User implements UserDetails {
         this.password = password;
     }
 
+    public User(@NonNull String username, @NonNull String email, @NonNull String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+        this.username = null;
+        this.email = null;
+        this.password = null;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority("ROLE_USER"));
@@ -70,4 +79,69 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public String getPhone_number() {
+        return this.phone_number;
+    }
+
+    public @NonNull String getUsername() {
+        return this.username;
+    }
+
+    public @NonNull String getEmail() {
+        return this.email;
+    }
+
+    public @NonNull String getPassword() {
+        return this.password;
+    }
+
+    public Role getRole() {
+        return this.role;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public void setUsername(@NonNull String username) {
+        this.username = username;
+    }
+
+    public void setEmail(@NonNull String email) {
+        this.email = email;
+    }
+
+    public void setPassword(@NonNull String password) {
+        this.password = password;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
 }
